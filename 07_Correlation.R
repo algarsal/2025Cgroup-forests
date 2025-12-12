43bdb5574f676f0f728ec1b6011714a58144db69
hejase <hasanhejase@gmail.com>
2025-12-12 11:59:25 +0100

    07 Correlation

--- 07_Correlation.R
+++ 07_Correlation.R
@@ -1,3 +1,15 @@
+#Correlation between the traits
+
+library(GGally)
+ggpairs(foret, columns = 6:10,
+        aes(color = For.type),
+        upper = list(
+          continuous = wrap("cor", size = 4)),
+        lower = list(
+          continuous = wrap("smooth", 
+                            alpha = 0.3, size = 0.5))) +
+  theme_minimal()
+
 # Keep only numeric variables from your dataset
 load("data/foret.Rdata")
 numeric_vars <- sapply(foret, is.numeric)
@@ -24,4 +36,65 @@ ggpairs(foret, columns = 4:8,
           continuous = wrap("smooth", 
                             alpha = 0.3, size = 0.5))) +
   theme_minimal()
+
+library(GGally)
+ggpairs(foret, columns = 9:13,
+        aes(color = For.type),
+        upper = list(
+          continuous = wrap("cor", size = 4)),
+        lower = list(
+          continuous = wrap("smooth", 
+                            alpha = 0.3, size = 0.5))) +
+  theme_minimal()
+
+library(GGally)
+ggpairs(foret, columns = 14:18,
+        aes(color = For.type),
+        upper = list(
+          continuous = wrap("cor", size = 4)),
+        lower = list(
+          continuous = wrap("smooth", 
+                            alpha = 0.3, size = 0.5))) +
+  theme_minimal()
+
+library(GGally)
+ggpairs(foret, columns = 19:23,
+        aes(color = For.type),
+        upper = list(
+          continuous = wrap("cor", size = 4)),
+        lower = list(
+          continuous = wrap("smooth", 
+                            alpha = 0.3, size = 0.5))) +
+  theme_minimal()
+
+library(GGally)
+ggpairs(foret, columns = 24:28,
+        aes(color = For.type),
+        upper = list(
+          continuous = wrap("cor", size = 4)),
+        lower = list(
+          continuous = wrap("smooth", 
+                            alpha = 0.3, size = 0.5))) +
+  theme_minimal()
+
+library(GGally)
+ggpairs(foret, columns = 29:33,
+        aes(color = For.type),
+        upper = list(
+          continuous = wrap("cor", size = 4)),
+        lower = list(
+          continuous = wrap("smooth", 
+                            alpha = 0.3, size = 0.5))) +
+  theme_minimal()
+
+library(GGally)
+ggpairs(foret, columns = 34:39,
+        aes(color = For.type),
+        upper = list(
+          continuous = wrap("cor", size = 4)),
+        lower = list(
+          continuous = wrap("smooth", 
+                            alpha = 0.3, size = 0.5))) +
+  theme_minimal()
+
 
